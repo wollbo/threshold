e210ece1d342b17737629bc88610229121308fde
Hilding Wollbo <wollbo@kth.se>
2020-11-10 16:26:30 +0100

    updated plotting slightly, new report

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ from sklearn.pipeline import make_pipeline
 from sklearn import metrics
 from data import core
 
-x, y = core.load_data("credit")
-p = sum(y)/len(y) # 70 % positive 30 % negative
+alphas = [1, 0.5, 2]
+
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
+
+x, y = core.load_data("breastcancer")
+p = sum(y)/len(y)  # 70 % positive 30 % negative
 x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 model = make_pipeline(StandardScaler(), LGBMClassifier())
@@ -19,24 +24,27 @@ pred = model.predict_proba(x_test)[:, 1]
 
 tpr, fpr, thres = metrics.roc_curve(y_test, pred)
 plt.plot(tpr, fpr)
+plt.ylabel('True positive rate')
+plt.xlabel('False positive rate')
+plt.show()
+
+span = np.linspace(start=0.001, stop=1, endpoint=False)  # cost function not defined at 0, 1
+plt.hist(pred[y_test == 1], density=True, bins=50)
+plt.hist(pred[y_test == 0], density=True, bins=50)
+
+
+for idx, a in enumerate(alphas):  # Basic plotting structure
+    plt.axvline(x=core.kl_threshold(p, alpha=a), c=colors[idx])
+plt.legend(labels=[f'λ = {a}' for a in alphas])
 plt.show()
 
 
-span = np.linspace(start=0.001, stop=1, endpoint=False)
-plt.hist(pred[y_test == 1], density=True, bins=30)
-plt.hist(pred[y_test == 0], density=True, bins=30)
-plt.axvline(x=core.kl_threshold(p, alpha=1), c='red')
-plt.axvline(x=core.kl_threshold(p, alpha=2), c='blue')
-plt.axvline(x=core.kl_threshold(p, alpha=3), c='green')
-
-plt.legend(labels=['λ = 1', 'λ = 2', 'λ = 3'])
+for idx, a in enumerate(alphas):  # Basic plotting structure
+    plt.plot(span, core.kl_cost(span, p, alpha=a), c=colors[idx])
+plt.legend(labels=[f'λ = {a}' for a in alphas])
 plt.show()
 
-plt.plot(span, core.kl_cost(span, p, alpha=1), c='red')
-plt.plot(span, core.kl_cost(span, p, alpha=2), c='blue')
-plt.plot(span, core.kl_cost(span, p, alpha=3), c='green')
-plt.legend(labels=['λ = 1', 'λ = 2', 'λ = 3'])
-plt.show()
+
 # training data predictions
 # plt.hist(model.predict_proba(x_train)[:, 1][y_train == 0], bins=50)
 # plt.hist(model.predict_proba(x_train)[:, 1][y_train == 1], bins=50)
