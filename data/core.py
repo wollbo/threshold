0882be1b3eb6573429e4dd4b977773c148d8a157
Hilding Wollbo <wollbo@kth.se>
2020-11-19 11:03:14 +0100

    updated report and example

--- data/core.py
+++ data/core.py
@@ -32,3 +32,29 @@ def kl_cost(span, p=0.5, alpha=1):
 def normalize_hist(values, hist):
     for item in hist:
         item.set_height(item.get_height()/sum(values))
+
+
+def exponential_mixture(span, p=0.5, l0=4, l1=6):
+    f0 = np.exp(-l0 * span) * l0 / (1 - np.exp(-l0))
+    f1 = - np.exp(l1 * span) * l1 / (1 - np.exp(l1))
+    return p * f1 + (1-p) * f0, (1-p)*f0, p*f1
+
+
+#def exponential_threshold(p=0.5, alpha=1, l1=5, l0=4):
+#    return 1 + np.log((p / (p + alpha * (1-p))) ** (1/(l0+l1)))
+
+
+def exponential_threshold(p=0.5, alpha=1, l=5, epsilon=0.001):
+    q = 0.5
+    q_new = 1
+    while abs(q-q_new)>epsilon:
+        q = q_new
+        f, d = exponential_integral(p, q, l)
+        q_new = q - (f-kl_threshold(p, alpha))/d
+    return q
+
+
+def exponential_integral(p=0.5, q=0.5, l=5):
+    f_val = np.exp(-l*q) * (np.exp(l)-np.exp(l*q)) * (p*(np.exp(l*q)-1) + 1) / (np.exp(l)-1)
+    df_val = np.exp(-l*q) * (l*np.exp(l)*(p-1) - l*p*np.exp(2*l*q)) / (np.exp(l)-1)
+    return f_val, df_val
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from lightgbm import LGBMClassifier
 import sys
@@ -9,12 +10,25 @@ from sklearn.pipeline import make_pipeline
 from sklearn import metrics
 from data import core
 
+pgf = False
+dataset = "credit"
 alphas = [1, 0.5, 2]
+
+if pgf:
+    matplotlib.use("pgf")
+    matplotlib.rcParams.update({
+        "pgf.texsystem": "pdflatex",
+        'font.family': 'serif',
+        'text.usetex': True,
+        'pgf.rcfonts': False,
+    })
+
+
 
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
 
-x, y = core.load_data("breastcancer")
+x, y = core.load_data(dataset)
 p = sum(y)/len(y)  # 70 % positive 30 % negative
 x_train, x_test, y_train, y_test = train_test_split(x, y)
 
@@ -22,11 +36,24 @@ model = make_pipeline(StandardScaler(), LGBMClassifier())
 model.fit(x_train, y_train)
 pred = model.predict_proba(x_test)[:, 1]
 
-tpr, fpr, thres = metrics.roc_curve(y_test, pred)
-plt.plot(tpr, fpr)
-plt.ylabel('True positive rate')
-plt.xlabel('False positive rate')
+#tpr, fpr, thres = metrics.roc_curve(y_test, pred)
+#plt.plot(tpr, fpr)
+#plt.ylabel('True positive rate')
+#plt.xlabel('False positive rate')
+#plt.show()
+p = 0.2
+l = 5
+alpha = 2
+
+span = np.linspace(start=0.0, stop=1.0, endpoint=True)
+mix, f0, f1 = core.exponential_mixture(span, p=p, l1=l, l0=l)
+plt.plot(span, mix)
+plt.plot(span, f0)
+plt.plot(span, f1)
+plt.axvline(x=core.exponential_threshold(p, alpha=alpha, l=l))
+print(1-core.kl_threshold(p, alpha))
 plt.show()
+sys.exit(0)
 
 span = np.linspace(start=0.001, stop=1, endpoint=False)  # cost function not defined at 0, 1
 plt.hist(pred[y_test == 1], density=True, bins=50)
@@ -35,14 +62,15 @@ plt.hist(pred[y_test == 0], density=True, bins=50)
 
 for idx, a in enumerate(alphas):  # Basic plotting structure
     plt.axvline(x=core.kl_threshold(p, alpha=a), c=colors[idx])
-plt.legend(labels=[f'λ = {a}' for a in alphas])
-plt.show()
+plt.legend(labels=[f'$\lambda = {a}$' for a in alphas])
+#plt.show()
+plt.savefig(f'report/figures/thresholds_{dataset}.pgf')
 
 
-for idx, a in enumerate(alphas):  # Basic plotting structure
-    plt.plot(span, core.kl_cost(span, p, alpha=a), c=colors[idx])
-plt.legend(labels=[f'λ = {a}' for a in alphas])
-plt.show()
+#for idx, a in enumerate(alphas):  # Basic plotting structure
+#    plt.plot(span, core.kl_cost(span, p, alpha=a), c=colors[idx])
+#plt.legend(labels=[f'λ = {a}' for a in alphas])
+#plt.show()
 
 
 # training data predictions
