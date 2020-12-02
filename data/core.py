e432563ea87f9d0fcabfd9410a9faa7969ab9cb4
Hilding Wollbo <wollbo@kth.se>
2020-12-02 16:13:55 +0100

    added argparser, new KDD dataset, updated report

--- data/core.py
+++ data/core.py
@@ -1,24 +1,51 @@
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+import sqlite3
 import sys
 from sklearn import datasets, metrics
 
 
 def load_data(dataset="breastcancer", return_X_y=True):
-
     if dataset == "breastcancer":
         (data, target) = datasets.load_breast_cancer(return_X_y=return_X_y)
-    elif dataset == "digits":
-        (data, target) = datasets.load_digits(return_X_y=return_X_y)
     elif dataset == "credit":
         cdt = datasets.fetch_openml(name='credit-g')
         data = cdt["data"]
         target = np.array([1 if item == 'good' else 0 for item in cdt["target"]])
+    elif dataset == "orange-small":  # https://www.kdd.org/kdd-cup/view/kdd-cup-2009/Data
+        # using training dataset as complete dataset due to true test labels not being available
+        raw = pd.read_csv('data/dataset/orange-small/orange_small_train.data', sep="\t")
+        data = clean_data(raw)
+        target = pd.read_csv('data/dataset/orange-small/labels/orange_small_train_churn.labels', sep="\t", header=None)
+        target.to_numpy()
+        target[target < 0] = 0
+        target = np.squeeze(target)
+    elif dataset == "orange-large":  # will probably not be possible to train models in a reasonable amount of time
+        print("Not implemented yet!")
+        sys.exit(1)  # add functionality for reading data batches and cleaning with SQL
     else:
         print("Invalid dataset")
         sys.exit(1)
     return data, target
+
+
+def clean_data(data):
+    """ Takes a DataFrame with incomplete values and fills in NaN fields, dropping constant columns, returning array """
+
+    data.fillna(value=data.mean(), inplace=True)  # fills numerical NaN values with mean
+    data.fillna(value=str(0), inplace=True)  # fills categorical NaN values with zero string
+    data.loc[:, (data != data.iloc[0]).any()]  # drops constant columns, may cause errors in train/test indexing
+    categorical = data.select_dtypes(['object']).columns
+    for cat in categorical:
+        data[cat] = data[cat].astype('category')
+    data[categorical] = data[categorical].apply(lambda x: x.cat.codes)
+    return data.to_numpy()
+
+
+def read_clean_sql(data):
+    """ Uses SQLite to read and clean larger datasets """
+    pass
 
 
 def kl_threshold(p, alpha):
@@ -40,14 +67,11 @@ def exponential_mixture(span, p=0.5, l0=4, l1=6):
     return p * f1 + (1-p) * f0, (1-p)*f0, p*f1
 
 
-#def exponential_threshold(p=0.5, alpha=1, l1=5, l0=4):
-#    return 1 + np.log((p / (p + alpha * (1-p))) ** (1/(l0+l1)))
-
-
 def exponential_threshold(p=0.5, alpha=1, l=5, epsilon=0.001):
+    """Implementation of Newtons Method for finding q with exponential integral"""
     q = 0.5
     q_new = 1
-    while abs(q-q_new)>epsilon:
+    while abs(q-q_new) > epsilon:
         q = q_new
         f, d = exponential_integral(p, q, l)
         q_new = q - (f-kl_threshold(p, alpha))/d
@@ -55,6 +79,7 @@ def exponential_threshold(p=0.5, alpha=1, l=5, epsilon=0.001):
 
 
 def exponential_integral(p=0.5, q=0.5, l=5):
+    """Cost function for calculating I = p / (p + l * (1-p)) and its derivative"""
     f_val = np.exp(-l*q) * (np.exp(l)-np.exp(l*q)) * (p*(np.exp(l*q)-1) + 1) / (np.exp(l)-1)
     df_val = np.exp(-l*q) * (l*np.exp(l)*(p-1) - l*p*np.exp(2*l*q)) / (np.exp(l)-1)
     return f_val, df_val
--- main.py
+++ main.py
@@ -1,79 +1,102 @@
 import numpy as np
+import argparse
 import matplotlib
 import matplotlib.pyplot as plt
 from lightgbm import LGBMClassifier
 import sys
-from sklearn.utils import class_weight
-from sklearn.model_selection import train_test_split
+from sklearn import metrics, model_selection
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from sklearn import metrics
 from data import core
 
-pgf = False
-dataset = "credit"
-alphas = [1, 0.5, 2]
 
-if pgf:
-    matplotlib.use("pgf")
-    matplotlib.rcParams.update({
-        "pgf.texsystem": "pdflatex",
-        'font.family': 'serif',
-        'text.usetex': True,
-        'pgf.rcfonts': False,
-    })
+def arg_parse():
+    """Parsing input arguments"""
+    parser = argparse.ArgumentParser(description='KL-D optimal threshold selection')
 
+    # Main experiment parameters
+    parser.add_argument('-d', nargs=1, type=str, default="breastcancer", help="Dataset")
+    parser.add_argument('-p', dest='plot', default=False, action='store_true', help="Saving plots")
+    parser.add_argument('-l', nargs=1, type=list, default=[1, 2, 0.5], help="Evaluated FP/FN ratios")
+    parser.add_argument('-e', nargs=1, type=int, default=[0], help="Exponential prediction")
+    parser.add_argument('-r', dest='roc', default=False, action='store_true', help="Plot ROC curve")
+    parser.add_argument('-c', dest='cost', default=False, action='store_true', help="Plot cost function")
 
+    # ML-model parameters
 
-prop_cycle = plt.rcParams['axes.prop_cycle']
-colors = prop_cycle.by_key()['color']
-
-x, y = core.load_data(dataset)
-p = sum(y)/len(y)  # 70 % positive 30 % negative
-x_train, x_test, y_train, y_test = train_test_split(x, y)
-
-model = make_pipeline(StandardScaler(), LGBMClassifier())
-model.fit(x_train, y_train)
-pred = model.predict_proba(x_test)[:, 1]
-
-#tpr, fpr, thres = metrics.roc_curve(y_test, pred)
-#plt.plot(tpr, fpr)
-#plt.ylabel('True positive rate')
-#plt.xlabel('False positive rate')
-#plt.show()
-p = 0.2
-l = 5
-alpha = 2
-
-span = np.linspace(start=0.0, stop=1.0, endpoint=True)
-mix, f0, f1 = core.exponential_mixture(span, p=p, l1=l, l0=l)
-plt.plot(span, mix)
-plt.plot(span, f0)
-plt.plot(span, f1)
-plt.axvline(x=core.exponential_threshold(p, alpha=alpha, l=l))
-print(1-core.kl_threshold(p, alpha))
-plt.show()
-sys.exit(0)
-
-span = np.linspace(start=0.001, stop=1, endpoint=False)  # cost function not defined at 0, 1
-plt.hist(pred[y_test == 1], density=True, bins=50)
-plt.hist(pred[y_test == 0], density=True, bins=50)
+    return parser.parse_args()
 
 
-for idx, a in enumerate(alphas):  # Basic plotting structure
-    plt.axvline(x=core.kl_threshold(p, alpha=a), c=colors[idx])
-plt.legend(labels=[f'$\lambda = {a}$' for a in alphas])
-#plt.show()
-plt.savefig(f'report/figures/thresholds_{dataset}.pgf')
+if __name__ == '__main__':
+
+    args = arg_parse()
+
+    dataset = args.d[0]  # dataset
+    lambdas = args.l  # list of evaluated fp/fn ratios
+    exp_pred = args.e[0]  # exponential test prediction parameter
+    roc = args.roc  # plot roc curve of prediction
+    pgf = args.plot
+    cost = args.cost
+
+    if pgf:
+        matplotlib.use("pgf")
+        matplotlib.rcParams.update({
+            "pgf.texsystem": "pdflatex",
+            'font.family': 'serif',
+            'text.usetex': True,
+            'pgf.rcfonts': False,
+        })
+
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
+
+    x, y = core.load_data(dataset)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y)
+
+    p = sum(y_train) / len(y_train)  # note: varying data structure between datasets
+
+    model = make_pipeline(StandardScaler(), LGBMClassifier())
+    model.fit(x_train, y_train)
+    pred = model.predict_proba(x_test)[:, 1]  # Model outputs both class probabilities
+
+    if roc:
+        tpr, fpr, thres = metrics.roc_curve(y_test, pred)
+        plt.plot(tpr, fpr)
+        plt.ylabel('True positive rate')
+        plt.xlabel('False positive rate')
+        plt.show()
+
+    if exp_pred:
+        span = np.linspace(start=0.0, stop=1.0, endpoint=True)
+        mix, f0, f1 = core.exponential_mixture(span, p=p, l1=exp_pred, l0=exp_pred)
+        plt.plot(span, mix)
+        plt.plot(span, f0)
+        plt.plot(span, f1)
+        for idx, a in enumerate(lambdas):
+            plt.axvline(x=core.exponential_threshold(p, alpha=a, l=exp_pred), c=colors[-idx], lw=2)
+        plt.legend(labels=[f'$\lambda = {a}$' for a in lambdas])
+        print(f'p = {p}')
+        print(1-core.kl_threshold(p, lambdas[0]))
+        plt.savefig(f'report/figures/thresholds_{dataset}_exponential_{exp_pred}.pgf') if pgf else plt.show()
+        plt.close(fig='all')
+
+    span = np.linspace(start=0.001, stop=1, endpoint=False)  # cost function not defined at endpoints (0, 1)
+    plt.hist(pred[y_test == 1], density=True, alpha=0.65, bins=50)
+    plt.hist(pred[y_test == 0], density=True, alpha=0.65, bins=50)
+
+    for idx, a in enumerate(lambdas):  # Basic plotting structure
+        plt.axvline(x=core.kl_threshold(p, alpha=a), c=colors[-idx], lw=2)
+    plt.legend(labels=[f'$\lambda = {a}$' for a in lambdas])
+    plt.savefig(f'report/figures/thresholds_{dataset}.pgf') if pgf else plt.show()
+
+    if cost:
+        for idx, a in enumerate(lambdas):  # Basic plotting structure
+            plt.plot(span, core.kl_cost(span, p, alpha=a), c=colors[-idx])
+        plt.legend(labels=[f'λ = {a}' for a in lambdas])
+        plt.show()
 
 
-#for idx, a in enumerate(alphas):  # Basic plotting structure
-#    plt.plot(span, core.kl_cost(span, p, alpha=a), c=colors[idx])
-#plt.legend(labels=[f'λ = {a}' for a in alphas])
-#plt.show()
-
-
-# training data predictions
-# plt.hist(model.predict_proba(x_train)[:, 1][y_train == 0], bins=50)
-# plt.hist(model.predict_proba(x_train)[:, 1][y_train == 1], bins=50)
+    # training data predictions
+    # plt.hist(model.predict_proba(x_train)[:, 1][y_train == 0], bins=50)
+    # plt.hist(model.predict_proba(x_train)[:, 1][y_train == 1], bins=50)
 
